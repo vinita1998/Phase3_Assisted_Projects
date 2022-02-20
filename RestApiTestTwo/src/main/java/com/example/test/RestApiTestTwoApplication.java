5ed175ca6d88d4241cc5337e7ce4f386c77e2c42
Vinita <Vinita@LAPTOP-T619IKD9>
2022-02-21 00:47:16 +0530

    commit

---
+++ RestApiTestTwo/src/main/java/com/example/test/RestApiTestTwoApplication.java
@@ -0,0 +1,15 @@
+package com.example.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTestTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiTestTwoApplication.class, args);
+    }
+
+}
+
+
