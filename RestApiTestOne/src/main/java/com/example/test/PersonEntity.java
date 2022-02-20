0e8de1b0e60e4f9af2f6c5236db9d83074dfc543
Vinita <Vinita@LAPTOP-T619IKD9>
2022-02-21 00:46:32 +0530

    commit

---
+++ RestApiTestOne/src/main/java/com/example/test/PersonEntity.java
@@ -0,0 +1,9 @@
+package com.example.test;
+
+public interface PersonEntity {
+
+	Object getPersonId();
+
+	Object getAge();
+
+}
---
+++ RestApiTestOne/src/main/java/com/example/test/PersonService.java
@@ -0,0 +1,35 @@
+package com.example.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class PersonService {
+    
+    @Autowired
+    PersonRepository personRepository;
+    
+    RestTemplate restTemplate = new RestTemplate();
+        
+    public PersonRepository getPerson(int personId){
+        final String uri = "http://localhost:8082/webapitwo/hobby/{personId}";
+        
+         Map<String, Integer> params = new HashMap<String, Integer>();
+         params.put("personId", personId);
+         
+         String result = restTemplate.getForObject(uri, String.class, params);
+         PersonEntity pe=personRepository.findById(personId).get();
+		return personRepository;
+        
+         
+    }   
+    public void addPerson(PersonEntity pe){
+        personRepository.save(pe);
+    }
+}
+
+
