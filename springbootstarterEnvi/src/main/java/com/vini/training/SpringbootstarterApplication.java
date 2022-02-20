a6556ca3465b3aa9c9b3ddccc853d6c6ed7b52c0
Vinita <Vinita@LAPTOP-T619IKD9>
2022-02-21 00:34:14 +0530

    commit

---
+++ springbootstarterEnvi/src/main/java/com/vini/training/SpringbootstarterApplication.java
@@ -0,0 +1,13 @@
+package com.vini.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootstarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootstarterApplication.class, args);
+	}
+
+}
