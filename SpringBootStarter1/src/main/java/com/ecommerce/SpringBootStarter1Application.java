e511bf126c4c895572351c6d4803a2d91b02248e
Vinita <Vinita@LAPTOP-T619IKD9>
2022-02-21 00:38:48 +0530

    commit

---
+++ SpringBootStarter1/src/main/java/com/ecommerce/SpringBootStarter1Application.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarter1Application.class, args);
+	}
+
+}
